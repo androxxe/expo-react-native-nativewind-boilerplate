9682955583756813ac65ea25cf210307f5c8d61e
androxxe <andriopratama16@gmail.com>
2023-11-22 01:28:20 +0700

    feat: add react-query

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Routes } from "./src/routes";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
+const queryClient = new QueryClient()
+
 export default function App() {
   return (
-    <GestureHandlerRootView className="flex-1">
-      <Routes />
-    </GestureHandlerRootView>
+    <QueryClientProvider client={queryClient}>
+      <GestureHandlerRootView className="flex-1">
+        <Routes />
+      </GestureHandlerRootView>
+    </QueryClientProvider>
   );
 }
